03b4f4c97c5123c88d513a0409fa25f0a6c59116
myrzabek2022 <myrzabek2022@gmail.com>
2022-03-01 15:41:35 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        1,1,2,3,5,8,13,21,34...
+        Scanner scan = new Scanner(System.in);
+        System.out.println("bir san jazynyz:");
+        int input = scan.nextInt();
+        int san1 = 0;
+        int san2 = 1;
+        int total;
+        for (int i = 0; i < input; i++) {
+            total = san1+san2;
+            san1 = san2;
+            san2=total;
+            System.out.print(san1+",");
+            System.out.println();
+            System.out.print(san2+",");
+            System.out.println();
+            System.out.print(total+",");
+        }
+    }
+}
